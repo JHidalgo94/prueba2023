d1278c6e6aceeeb4f56807ad4bd579469cd7c419
JHidalgo94 <124218640+JHidalgo94@users.noreply.github.com>
2023-02-22 21:05:09 -0600

    clases

---
+++ Proyecto prueba/Entidades/Login.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Login
+    {
+        public string codigousuario { get; set; }
+
+        public string contrasena { get; set; }
+
+        public string rol { get; set; }
+
+        public Login()
+        {
+        }
+
+        public Login(string codigousuario, string contrasena, string rol)
+        {
+            this.codigousuario = codigousuario;
+            this.contrasena = contrasena;
+            this.rol = rol;
+        }
+    }
+}
